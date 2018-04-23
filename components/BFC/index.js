f6d278a24f4297df87bf70e7c096ef915d83688f
lanmingming <lanmingming@meituan.com>
2018-04-23 15:59:50 +0800

    feat: bfc

---
+++ components/BFC/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file BFC
+ */
+import React from 'react'
+
+import './index.scss'
+
+export default props => (
+  <div className="bfc-wrapper">
+    <div className="element">hello BFC</div>
+  </div>
+)
